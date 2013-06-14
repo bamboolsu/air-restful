235803c117c4db77eb180195c6f0dc58f2e5c507
Eric Han <feuyeux@gmail.com>
2013-06-15 03:06:11 +0800

    initialize version 0.1.0

---
+++ src/main/java/org/feuyeux/air/restful/dao/AbcDao.java
@@ -0,0 +1,16 @@
+package org.feuyeux.air.restful.dao;
+
+import org.feuyeux.air.restful.model.AbcEntity;
+import org.springframework.stereotype.Repository;
+
+/**
+ * JPA DAO Tier Bean
+ * 
+ * @author feuyeux@gmail.com
+ * 05/08/2012
+ * @version 0.1.0
+ * @since 0.0.1
+ */
+@Repository
+public class AbcDao extends GenericDao<AbcEntity, Integer> {
+}
